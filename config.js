3a36cd5d41c86f9af35f1cd11c2509a131e77457
smartychuks <Smirhty@gmail.com>
2023-01-07 05:47:28 +0100

    alpha

---
+++ config.js
@@ -0,0 +1,12 @@
+// Mainnet DAI contract address
+const DAI = "0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063";
+// an address that has DAI
+const DAI_WHALE = "0xdfD74E3752c187c4BA899756238C76cbEEfa954B";
+
+// Mainnet Pool contract address
+const POOL_ADDRESS_PROVIDER = "0xa97684ead0e402dc232d5a977953df7ecbab3cdb";
+module.exports = {
+    DAI,
+    DAI_WHALE,
+    POOL_ADDRESS_PROVIDER,
+};
